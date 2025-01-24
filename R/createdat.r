43778741bb21919bafed35a69a39ba078253775e
nvilloria <nvilloria@gmail.com>
2025-01-24 07:52:45 -0600

    Removing functions that are not used

--- R/createdat.r
+++
@@ -1,9 +0,0 @@
-createdat <- function(mapfile, setfile, datfile, stdprm, dir){
-    aggdat(mapfile, setfile, datfile, dir)
-    lusepar(mapfile, setfile, datfile, stdprm, dir)
-    gtapview(dir)
-    inputfiles <- c("aggcheck.cmf","aggdat.cmf","aggpar.cmf",
-                    "lusepar.cmf","gtapview.cmf","gmap.har")
-    zip(zipfile=paste("./",dir,"/inputs.zip",sep=""),files=inputfiles)
-    file.remove(c(inputfiles,"gmap.sti","gmap.log"))
-    }
