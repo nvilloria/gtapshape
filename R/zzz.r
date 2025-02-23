def6eeea07d40f5a14fdfa1b1a753a8b0ff1873f
nvilloria <nvilloria@gmail.com>
2025-02-23 10:14:33 -0600

    Delete zzz.r

--- R/zzz.r
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-
-    packageStartupMessage("\n\nWelcome to gtapshape! Run setup_gtapshape() the first time you use this package. You can also run setup_gtapshape() to check that the needed data and programs are installed.\n")
-    }
