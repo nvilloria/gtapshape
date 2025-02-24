f3aaf43bee0aab5216f45218bda09b144f750aff
nvilloria <nvilloria@gmail.com>
2025-02-24 14:33:27 -0600

    Data documentation

--- R/GTAP.ruminant.prices.R
+++ R/GTAP.ruminant.prices.R
@@ -2,10 +2,12 @@
 #' @description This dataset contains GTAP ruminant prices.
 #' @format A data frame with the following columns:
 #'   \describe{
-#'     \item{year}{Year of data}
-#'     \item{price}{Ruminant price}
+#'     \item{iso3}{Country}
+#'     \item{use}{Ruminant species}
+#'     \item{unit}{USD/head}
+#'     \item{value}{Value}
 #'   }
-#' @source The script to generate this dataset can be found in the
+#' @source "The script to generate this dataset can be found in the
 #'   `data-raw` folder on GitHub:
-#'   \url{https://github.com/nvilloria/gtapshape/tree/main/data-raw}
+#'   \url{https://github.com/nvilloria/gtapshape/tree/main/data-raw}"
 "GTAP.ruminant.prices"
