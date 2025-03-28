d1d64c5a81167e5641c76057ae9fe485a9d35ddd
nvilloria <nvilloria@gmail.com>
2025-03-28 14:48:46 -0500

    Update setup_gtapshape.R

--- R/setup_gtapshape.R
+++ R/setup_gtapshape.R
@@ -7,7 +7,7 @@ setup_gtapshape<- function() {
         ## Use this for testing:
         ## getrawdata(url="https://www.gtap.agecon.purdue.edu/uploads/temp/FlexSpatialAggTest.zip")
         ## Use this for the actual data:
-       getrawdata(url="https://www.gtap.agecon.purdue.edu/uploads/temp/FlexSpatialAgg.zip")
+        getrawdata(url="https://www.gtap.agecon.purdue.edu/uploads/temp/FlexSpatialAgg.zip")
     }else{
         cat("A folder named raw_data exists. This suggests that the raw data needed to construct \nthe GIS layers to split the national land use and land cover data is present. If \nfor any reason this is not the case and these data are really needed, run the \nfunction getrawdata().\n\n")
     }
