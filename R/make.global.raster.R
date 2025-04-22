77d2e9b64c34edccda3eff743687a6a222e07f6b
Micah Cameron Harp <mcameronharp@ksu.edu>
2025-04-22 10:33:17 -0500

    Update make.global.raster.R
    Added export tag so it can be used in gtapluco2

--- R/make.global.raster.R
+++ R/make.global.raster.R
@@ -1,11 +1,14 @@
 #' Make a Global Raster
 #'
-#'This empty raster with has a set of properties that are imposed on
+#' This empty raster with has a set of properties that are imposed on
 #' all the rasters in the package in order to ensure that they align
 #' once they are converted to dataframes. This could be more directly
 #' achieved using the package raster, but because the sheer number of
 #' raster files (and their sheer sizes) working with the rasters
 #' directly is impractical.
+#'
+#' @returns A raster.
+#' @export
 make.global.raster <- function(){
     terra::rast(xmin = -180, xmax = 180, ymin = -90, ymax = 90,
                 resolution = c(0.08333333, 0.08333333),
